e3c606e78322320ad952b58157671a24e0631dfc
Shubham Mathur <shumathu@redhat.com>
2020-11-06 19:06:48 +0530

    fix: Remove ctrl/cmd+click text and functionality  (#174)
    * fix: Remove ctrl/cmd+click
    
    * add preventDefault

--- src/app/stack/utils/handle-url.ts
+++ src/app/stack/utils/handle-url.ts
@@ -1,14 +1,13 @@
-
 export class HandleUrl {
-
     public open(event, url = null) {
-        let elem: HTMLElement = (<HTMLElement>event.target);
-        if (elem && elem.attributes && elem.attributes.getNamedItem('href')) {
+        event.preventDefault();
+        event.stopPropagation();
+        let elem: HTMLElement = <HTMLElement>event.target;
+        if (elem && elem.attributes && elem.attributes.getNamedItem("href")) {
             let url = event.target.href;
             parent.postMessage(url, "*");
         } else if (url) {
             parent.postMessage(url, "*");
         }
-
     }
 }
