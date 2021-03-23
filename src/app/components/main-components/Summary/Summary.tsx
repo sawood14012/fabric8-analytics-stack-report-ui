78597ccd198837d68fbd04029aef4a0c75e23827
sawood14012 <msawood@redhat.com>
2021-03-23 09:24:23 +0530

    View first card

--- src/app/components/main-components/Summary/Summary.tsx
+++ src/app/components/main-components/Summary/Summary.tsx
@@ -3,14 +3,37 @@ import {
   Card,
   CardTitle,
   CardBody,
-  CardFooter,
-  TextVariants,
+  Split,
+  SplitItem,
   TextContent,
   Text,
+  Divider,
+  Flex,
+  FlexItem,
+  Grid,
+  GridItem,
+  Title,
 } from '@patternfly/react-core';
 import './Summary.scss';
+import {
+  CubeIcon,
+  SecurityIcon,
+  FileAltIcon,
+  ZoneIcon,
+  ShareSquareIcon,
+  DownloadIcon,
+  ExclamationCircleIcon,
+} from '@patternfly/react-icons';
+import SignUp from '../../shared-components/synk-signup/synk-signup';
+import PoweredBySynk from '../../shared-components/powerd-by/powerd-by';
+import BTSynktoken from '../../shared-components/synk-token-button/synk-token-button';
 
 class SummaryCard extends React.Component {
+  showsignup = false;
+
+  signUpText =
+    'Sign up for a free Snyk account to find out about the vulnerabilities that have been found, and whether any have a publicly known exploit';
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(props: any) {
     super(props);
@@ -21,12 +44,174 @@ class SummaryCard extends React.Component {
     return (
       <Card className="--pf-c-card--BoxShadow summary-of-the-stack">
         <CardTitle className="">
-          <TextContent>
-            <Text component={TextVariants.h2}>Summary of the stack </Text>
-          </TextContent>
+          <Flex>
+            <FlexItem>
+              <TextContent>
+                <Text className="heading"> Summary of the stack </Text>
+              </TextContent>
+            </FlexItem>
+            <FlexItem>
+              <BTSynktoken isUUID={this.showsignup} />
+            </FlexItem>
+            <FlexItem align={{ default: 'alignRight' }}>
+              <ShareSquareIcon color="#6A6E73" />
+            </FlexItem>
+            <FlexItem>
+              <DownloadIcon color="#6A6E73" />
+            </FlexItem>
+          </Flex>
         </CardTitle>
-        <CardBody>Body</CardBody>
-        <CardFooter>Footer</CardFooter>
+        <Divider />
+        <CardBody>
+          <PoweredBySynk />
+          <SignUp isUUID={this.showsignup} />
+          <br />
+          <Grid hasGutter>
+            <GridItem span={6}>
+              <Flex>
+                <FlexItem>
+                  <CubeIcon color="#6A6E73" />
+                </FlexItem>
+                <FlexItem>
+                  <Title headingLevel="h6" size="md">
+                    Dependency Details
+                  </Title>
+                </FlexItem>
+              </Flex>
+              <Flex>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>Analysed dependencies:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        17
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>Transitive dependencies:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        112
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>Unknown dependencies:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        10
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+              </Flex>
+            </GridItem>
+            <GridItem span={6}>
+              <Flex>
+                <FlexItem>
+                  <SecurityIcon color="#6A6E73" />
+                </FlexItem>
+                <FlexItem>
+                  <Title headingLevel="h6" size="md">
+                    Security Issues
+                  </Title>
+                </FlexItem>
+                <FlexItem>
+                  <ExclamationCircleIcon color="#C9190B" />
+                </FlexItem>
+              </Flex>
+              <Flex>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>Total vulnerabilities:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        87
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>Vulnerable dependencies:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        18
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+              </Flex>
+            </GridItem>
+            <Divider component="div" />
+            <GridItem span={6}>
+              <Flex>
+                <FlexItem>
+                  <FileAltIcon color="#6A6E73" />
+                </FlexItem>
+                <FlexItem>
+                  <Title headingLevel="h6" size="md">
+                    Licences
+                  </Title>
+                </FlexItem>
+                <FlexItem>
+                  <ExclamationCircleIcon color="#C9190B" />
+                </FlexItem>
+              </Flex>
+              <Flex>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>License conflicts:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        2
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>Unknown licenses:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        1
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+              </Flex>
+            </GridItem>
+            <GridItem span={6}>
+              <Flex>
+                <FlexItem>
+                  <ZoneIcon color="#6A6E73" />
+                </FlexItem>
+                <FlexItem>
+                  <Title headingLevel="h6" size="md">
+                    Add-ons
+                  </Title>
+                </FlexItem>
+              </Flex>
+              <Flex direction={{ default: 'column' }}>
+                <FlexItem>
+                  <Split hasGutter>
+                    <SplitItem>Companion Dependencies:</SplitItem>
+                    <SplitItem>
+                      <Title headingLevel="h6" size="md">
+                        3
+                      </Title>
+                    </SplitItem>
+                  </Split>
+                </FlexItem>
+              </Flex>
+            </GridItem>
+          </Grid>
+        </CardBody>
       </Card>
     );
   }
---
+++ src/app/components/shared-components/synk-token-button/synk-token-button.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import {
+  Button,
+  Modal,
+  Form,
+  FormGroup,
+  TextInput,
+  FormHelperText,
+} from '@patternfly/react-core';
+import {
+  CheckSquareIcon,
+  ExclamationCircleIcon,
+  ExclamationTriangleIcon,
+} from '@patternfly/react-icons';
+import PoweredBySynk from '../powerd-by/powerd-by';
+
+type Signprop = {
+  isUUID: boolean;
+};
+
+type State = {
+  isModalOpen: boolean;
+};
+
+type Modalprops = {
+  isModalOpen: boolean;
+  handleToggle: () => void;
+};
+
+const ModalComponent = ({ isModalOpen, handleToggle }: Modalprops) => {
+  const text =
+    ' to get your free Snyk token and get access to all premium fields.';
+
+  return (
+    <Modal
+      width="40%"
+      title="Snyk Token Submission"
+      isOpen={isModalOpen}
+      onClose={handleToggle}
+      actions={[
+        <Button key="confirm" variant="primary" onClick={handleToggle}>
+          Submit
+        </Button>,
+        <Button key="cancel" variant="link" onClick={handleToggle}>
+          Close
+        </Button>,
+      ]}
+    >
+      <Form>
+        <FormGroup
+          label="Snyk Token"
+          isRequired
+          fieldId="Synk token form group"
+          helperText={
+            <FormHelperText icon={<ExclamationCircleIcon />} isHidden={false}>
+              If a keyboard shortcut for Paste is not available, try using Edit
+              &gt; Paste
+            </FormHelperText>
+          }
+        >
+          <TextInput
+            isRequired
+            placeholder="Enter your Snyk token"
+            id="Snyk-token-text"
+            name="snyk-token-text"
+            value=""
+          />
+        </FormGroup>
+      </Form>
+      <br />
+      <PoweredBySynk />
+      <a
+        href="https://app.snyk.io/login?utm_campaign=Code-Ready-Analytics-2020&utm_source=code_ready&code_ready=FF1B53D9-57BE-4613-96D7-1D06066C38C9"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Sign up
+      </a>
+      {text}
+    </Modal>
+  );
+};
+
+class BTSynktoken extends React.Component<Signprop, State> {
+  handleModalToggle: () => void;
+
+  constructor(props: Signprop) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.handleModalToggle = () => {
+      this.setState(({ isModalOpen }) => ({
+        isModalOpen: !isModalOpen,
+      }));
+    };
+  }
+
+  render() {
+    const { isUUID } = this.props;
+    const { isModalOpen } = this.state;
+    if (isUUID === false) {
+      return [
+        <div>
+          <Button
+            icon={<ExclamationTriangleIcon />}
+            iconPosition="right"
+            className="mybutton"
+            isSmall
+            variant="warning"
+            onClick={this.handleModalToggle}
+          >
+            Snyk Token
+          </Button>
+          <ModalComponent
+            isModalOpen={isModalOpen}
+            handleToggle={this.handleModalToggle}
+          />
+        </div>,
+      ];
+    }
+    return [
+      <div>
+        <Button
+          icon={<CheckSquareIcon />}
+          iconPosition="right"
+          className="mybutton"
+          isSmall
+          variant="secondary"
+        >
+          Snyk Token
+        </Button>
+
+        <ModalComponent
+          isModalOpen={isModalOpen}
+          handleToggle={this.handleModalToggle}
+        />
+      </div>,
+    ];
+  }
+}
+
+export default BTSynktoken;
