c729034168ea0204a700d1df8e92762f3863bd09
Rafiu Jaman Mollah <rmollah@redhat.com>
2021-04-06 14:52:07 +0530

    a

---
+++ src/app/components/shared-components/addons-primary/add-ons.tsx
@@ -0,0 +1,71 @@
+import ReactDOM from 'react-dom';
+import "@patternfly/react-core/dist/styles/base.css";
+
+import React from 'react';
+import { Table, TableHeader, TableBody } from '@patternfly/react-table';
+import { ToggleGroup, ToggleGroupItem } from '@patternfly/react-core';
+
+class SimpleTable extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      choice: 'default'
+    };
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  handleItemClick(isSelected, event: any) {
+    this.setState({
+      choice: event.currentTarget.id
+    });
+  }
+
+  render() {
+    const { choice } = this.state;
+
+    const columns = ['Repositories', 'Branches', 'Pull requests', 'Workspaces', 'Last commit'];
+    const rows = [
+      ['Repository one', 'Branch one', 'PR one', 'Workspace one', 'Commit one'],
+      ['Repository two', 'Branch two', 'PR two', 'Workspace two', 'Commit two'],
+      ['Repository three', 'Branch three', 'PR three', 'Workspace three', 'Commit three']
+    ];
+
+    return (
+      <React.Fragment>
+        <ToggleGroup aria-label="Default with single selectable">
+          <ToggleGroupItem
+            text="Default"
+            buttonId="default"
+            isSelected={choice === 'default'}
+            onChange={this.handleItemClick}
+          />
+          <ToggleGroupItem
+            text="Compact"
+            buttonId="compact"
+            isSelected={choice === 'compact'}
+            onChange={this.handleItemClick}
+          />
+          <ToggleGroupItem
+            text="Compact without borders"
+            buttonId="compactBorderless"
+            isSelected={choice === 'compactBorderless'}
+            onChange={this.handleItemClick}
+          />
+        </ToggleGroup>
+        <Table
+          aria-label="Simple Table"
+          variant={choice !== 'default' ? 'compact' : null}
+          borders={choice !== 'compactBorderless'}
+          cells={columns}
+          rows={rows}
+        >
+          <TableHeader />
+          <TableBody />
+        </Table>
+      </React.Fragment>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<SimpleTable />, rootElement);
