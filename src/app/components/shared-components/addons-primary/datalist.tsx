3e28f234d489109898df5c645eb64a464a4ff368
Rafiu Jaman Mollah <rmollah@redhat.com>
2021-04-21 17:03:36 +0530

    Refactored datalist from class component to function component.

--- src/app/components/shared-components/addons-primary/datalist.tsx
+++ src/app/components/shared-components/addons-primary/datalist.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Dropdown,
@@ -31,141 +31,107 @@ type DataListState = {
 // eslint-disable-next-line
 type DataProps = Record<any, any>;
 
-class SelectableDataList extends React.Component<DataProps, DataListState> {
-  onToggle1: any;
+function SelectableDataList(props: any) {
+  const [isOpen1, setisOpen1] = useState(false);
+  const [isOpen2, setisOpen2] = useState(false);
+  const [selectedDataListItemId, setselectableDataListItemId] = useState('');
+  const [onToggle1, setonToggle1] = useState(false);
+  const [onToggle2, setonToggle2] = useState(false);
+  const [onSelect1, setonSelect1] = useState(false);
+  const [onSelect2, setonSelect2] = useState(false);
+  const [onSelectDataListItem, setonSelectDataListItem] = useState('');
+  const newItem = (id: any) => setonSelectDataListItem(id);
 
-  onToggle2: any;
-
-  onSelect1: any;
-
-  onSelect2: any;
-
-  onSelectDataListItem: any;
-
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      isOpen1: false,
-      isOpen2: false,
-      selectedDataListItemId: '',
-    };
-
-    this.onToggle1 = (isOpen1: any) => {
-      this.setState({ isOpen1 });
-    };
-
-    this.onToggle2 = (isOpen2: any) => {
-      this.setState({ isOpen2 });
-    };
-
-    this.onSelect1 = (event: any) => {
-      this.setState((isOpen1) => ({
-        isOpen1: !isOpen1,
-      }));
-    };
-
-    this.onSelect2 = (event: any) => {
-      this.setState((isOpen2) => ({
-        isOpen2: !isOpen2,
-      }));
-    };
-
-    this.onSelectDataListItem = (id: any) => {
-      this.setState({ selectedDataListItemId: id });
-    };
-  }
-
-  render() {
-    const { isOpen1, isOpen2, selectedDataListItemId } = this.state;
-    return (
-      <DataList
-        aria-label="selectable data list example"
-        selectedDataListItemId={selectedDataListItemId}
-        onSelectDataListItem={this.onSelectDataListItem}
-      >
-        <DataListItem aria-labelledby="selectable-action-item1" id="item1">
-          <DataListItemRow>
-            <DataListItemCells
-              dataListCells={[
-                <DataListCell key="primary content">
-                  <span id="selectable-action-item1">
-                    Single actionable Primary content
-                  </span>
-                  <DrawerStack />
-                </DataListCell>,
-                <DataListCell key="secondary content">
-                  <SingleLine />,
-                </DataListCell>,
+  return (
+    <DataList
+      aria-label="selectable data list example"
+      selectedDataListItemId={selectedDataListItemId}
+      onSelectDataListItem={() =>
+        setonSelectDataListItem(selectedDataListItemId)
+      }
+    >
+      <DataListItem aria-labelledby="selectable-action-item1" id="item1">
+        <DataListItemRow>
+          <DataListItemCells
+            dataListCells={[
+              <DataListCell key="primary content">
+                <span id="selectable-action-item1">
+                  Single actionable Primary content
+                </span>
+                <DrawerStack />
+              </DataListCell>,
+              <DataListCell key="secondary content">
+                <SingleLine />,
+              </DataListCell>,
+            ]}
+          />
+          <DataListAction
+            aria-labelledby="selectable-action-item1 selectable-action-action1"
+            id="selectable-action-action1"
+            aria-label="Actions"
+            isPlainButtonAction
+          >
+            <Dropdown
+              isPlain
+              position={DropdownPosition.right}
+              isOpen={isOpen1}
+              onSelect={() => setonSelect1(!isOpen1)}
+              toggle={<KebabToggle onToggle={() => setonToggle1(isOpen1)} />}
+              dropdownItems={[
+                <DropdownItem key="link">Link</DropdownItem>,
+                <DropdownItem key="action" component="button">
+                  Action
+                </DropdownItem>,
+                <DropdownItem key="disabled link" isDisabled>
+                  Disabled Link
+                </DropdownItem>,
               ]}
             />
-            <DataListAction
-              aria-labelledby="selectable-action-item1 selectable-action-action1"
-              id="selectable-action-action1"
-              aria-label="Actions"
-              isPlainButtonAction
-            >
-              <Dropdown
-                isPlain
-                position={DropdownPosition.right}
-                isOpen={isOpen1}
-                onSelect={this.onSelect1}
-                toggle={<KebabToggle onToggle={this.onToggle1} />}
-                dropdownItems={[
-                  <DropdownItem key="link">Link</DropdownItem>,
-                  <DropdownItem key="action" component="button">
-                    Action
-                  </DropdownItem>,
-                  <DropdownItem key="disabled link" isDisabled>
-                    Disabled Link
-                  </DropdownItem>,
-                ]}
-              />
-            </DataListAction>
-          </DataListItemRow>
-        </DataListItem>
+          </DataListAction>
+        </DataListItemRow>
+      </DataListItem>
 
-        <DataListItem aria-labelledby="selectable-actions-item2" id="item2">
-          <DataListItemRow>
-            <DataListItemCells
-              dataListCells={[
-                <DataListCell key="primary content">
-                  <span id="selectable-actions-item2">
-                    Selectable actions Primary content
-                  </span>
-                </DataListCell>,
-                <DataListCell key="secondary content">
-                  <SingleLine />,
-                </DataListCell>,
+      <DataListItem aria-labelledby="selectable-actions-item2" id="item2">
+        <DataListItemRow>
+          <DataListItemCells
+            dataListCells={[
+              <DataListCell key="primary content">
+                <span id="selectable-actions-item2">
+                  Selectable actions Primary content
+                </span>
+              </DataListCell>,
+              <DataListCell key="secondary content">
+                <SingleLine />,
+              </DataListCell>,
+            ]}
+          />
+          <DataListAction
+            aria-labelledby="selectable-actions-item2 selectable-actions-action2"
+            id="selectable-actions-action2"
+            aria-label="Actions"
+            isPlainButtonAction
+          >
+            <Dropdown
+              isPlain
+              position={DropdownPosition.right}
+              isOpen={isOpen2}
+              onSelect={() => setonSelect2(!isOpen2)}
+              toggle={<KebabToggle onToggle={() => setonToggle1(isOpen1)} />}
+              dropdownItems={[
+                <DropdownItem key="link">Link</DropdownItem>,
+                <DropdownItem key="action" component="button">
+                  Action
+                </DropdownItem>,
+                <DropdownItem key="disabled link" isDisabled>
+                  Disabled Link
+                </DropdownItem>,
               ]}
             />
-            <DataListAction
-              aria-labelledby="selectable-actions-item2 selectable-actions-action2"
-              id="selectable-actions-action2"
-              aria-label="Actions"
-              isPlainButtonAction
-            >
-              <Dropdown
-                isPlain
-                position={DropdownPosition.right}
-                isOpen={isOpen2}
-                onSelect={this.onSelect2}
-                toggle={<KebabToggle onToggle={this.onToggle2} />}
-                dropdownItems={[
-                  <DropdownItem key="link">Link</DropdownItem>,
-                  <DropdownItem key="action" component="button">
-                    Action
-                  </DropdownItem>,
-                  <DropdownItem key="disabled link" isDisabled>
-                    Disabled Link
-                  </DropdownItem>,
-                ]}
-              />
-            </DataListAction>
-          </DataListItemRow>
-        </DataListItem>
-      </DataList>
-    );
-  }
+          </DataListAction>
+        </DataListItemRow>
+      </DataListItem>
+    </DataList>
+  );
 }
 
 const SingleLine = () => {
