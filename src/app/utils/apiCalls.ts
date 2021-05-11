497fa5cc0dcbdbb2878b18c25800fb56aba7bc32
Meer Sawood <msawood@redhat.com>
2021-05-11 11:32:07 +0530

    linter fixes

--- src/app/utils/apiCalls.ts
+++ src/app/utils/apiCalls.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import Context from '../store/context';
 import { stageApiKey, stageApiUrl, localDevURL } from './constants';
 import { Logger } from './logger';
@@ -9,11 +9,13 @@ function GetStackDetails(requestId = 'ed16d5db91d64e8d8d0ea6d1ba6b727e') {
   const { globalState, globalDispatch } = useContext(Context);
   useEffect(() => {
     const url = `${stageApiUrl}/api/v2/stack-analyses/${requestId}?user_key=${stageApiKey}`;
+    Logger.log(url);
     const config = {
       headers: {
         UUID: '2527bcb9-9db1-4c8f-b304-c3b232ebd22f',
       },
     };
+    Logger.log(config);
     const devUrl = localDevURL;
     axios
       .get(devUrl)
