bd10714a91a85a57a4322d06aafb11ffb9f12b21
Rafiu Jaman Mollah <rmollah@redhat.com>
2021-06-01 19:19:07 +0530

    Added feedback function.

---
+++ src/app/utils/feedback.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+import axios, { AxiosResponse, AxiosError } from "axios";
+import { url } from "node:inspector";
+import { Logger } from "./logger";
+
+function submitFeedback(data: any) {
+    const url='https://recommender.api.openshift.io/api/v1/user-feedback';
+    axios({
+        method: 'post',
+        url: url,
+        headers: {}, 
+        data: {
+          foo: 'bar', // This is the body part
+        }
+      });
+}
